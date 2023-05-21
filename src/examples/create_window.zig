157859dcc0a037be4e3af78f9737c847fa0ccdb6
rimuspp <19101das@gmail.com>
2023-05-21 11:08:42 -0500

    Innit?

---
+++ src/examples/create_window.zig
@@ -0,0 +1,4 @@
+const std = @import("std");
+const xenia_client = @import("xenia_glass");
+
+pub fn main() !void {}
---
+++ src/server.zig
@@ -0,0 +1,4 @@
+const std = @import("std");
+const xev = @import("xev");
+
+pub fn main() !void {}
